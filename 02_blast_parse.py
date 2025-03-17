a0177903324124081368143e4d8764544b13eb02
Radovan Dvorsky <radovan.dvorsky@gmail.com>
2025-03-17 14:13:30 +0100

    Updated parsing of XML from BlastP to be able to analyse also new verisons of PDBAA

--- 02_blast_parse.py
+++ 02_blast_parse.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 import pandas as pd
 
 from Bio.Blast import NCBIXML
@@ -10,9 +10,9 @@ class PdbChain():
   def __init__(self, inp_string):
     self.pdb    = {}
     self.chains = {}
-    print(inp_string)
+    #print(inp_string)
     items = inp_string.split('|')
-    print(items)
+    #print(items)
     for ii, item in enumerate(items):
       if (ii%2)==1 and ii < 3: 
         pdbID = item.strip()
@@ -43,7 +43,7 @@ class PdbChain():
       out_string = out_string + ":" + self.chains[pdbID]
     return out_string
 
-def Parse_blast_xml(fname, searchID, headers, blTable, pdbTable):
+def Parse_blast_xml(fname, searchID, headers, blTable, pdbTable, new = False):
   blastTable_ID = 0
   pdbchTable_ID = 0
   
@@ -51,13 +51,35 @@ def Parse_blast_xml(fname, searchID, headers, blTable, pdbTable):
   blast_records = NCBIXML.parse(result_handle)
   for blast_record in blast_records:
     for ai, alignment in enumerate(blast_record.alignments):
-      pdb_chain   = PdbChain(alignment.title)
+      if new:
+        parts = alignment.title.split()
+        if len(parts)>1:
+          pdbCode = parts[1]
+          if len(pdbCode) > 4:
+            new_alignment_title = f"pdb|{pdbCode[:-1]}|{pdbCode[-1]}"
+            pdb_chain = PdbChain(new_alignment_title)
+          else:
+            print("The part for PDB-Chain does not have 5 character!!!")
+            sys.exit()  
+        else:
+          print("Something is wrong")
+          sys.exit()
+      else:
+        pdb_chain   = PdbChain(alignment.title)
+      
       pdbC_string = pdb_chain.Report()
+
+      hit_def = alignment.hit_def  # Get full description
+      match = re.search(r'\b([0-9][A-Za-z0-9]{3})\b', hit_def)  # Match PDB ID pattern (e.g., 8ORF)
+      if match:
+        match_pdb_id = match.group(1)  # Extract the matched PDB ID
+        #print("PDB", match_pdb_id)
+        #pdb_ids.append(match_pdb_id)
         
       for hsp in alignment.hsps:  
         blastTable_ID = blastTable_ID + 1
         blTable['ID_blast_search'].append(blastTable_ID)
-        blTable['SH3'].append(searchID)
+        blTable[dom_prot].append(searchID)
         blTable['title'].append(alignment.title)
         blTable['PDB_Chains'].append(pdbC_string)
         pdbItems = pdbC_string.split(':')
@@ -72,7 +94,8 @@ def Parse_blast_xml(fname, searchID, headers, blTable, pdbTable):
           pdbTable['ID'].append(pdbchTable_ID)
           pdbTable['PDB'].append(pdb_id)
           pdbTable['chains'].append(pdb_chain.chains[pdb_id])
-          pdbTable['search_id'].append(blastTable_ID)
+          #pdbTable['search_id'].append(blastTable_ID)
+          pdbTable['search_id'].append(f"blast:{searchID}")
 
           pdbTable['score'].append(getattr(hsp, 'score'))
           pdbTable['query'].append(getattr(hsp, 'query'))
@@ -81,7 +104,13 @@ def Parse_blast_xml(fname, searchID, headers, blTable, pdbTable):
           pdbTable['beg'].append(getattr(hsp, 'sbjct_start'))
           pdbTable['end'].append(getattr(hsp, 'sbjct_end'))
 
-blastColumns = ['ID_blast_search', 'SH3', 'title', 'PDB_Chains', 'length']
+
+
+
+dom_prot   = "raf"
+new_version = True
+
+blastColumns = ['ID_blast_search', dom_prot, 'title', 'PDB_Chains', 'length']
 blastHeaders = ['score', 'bits', 'expect', 'identities', 'positives', 'gaps', 'align_length', 'query', 'query_start', 'query_end', 'match', 'sbjct', 'sbjct_start', 'sbjct_end']
 blastTable = {column:[] for column in blastColumns + blastHeaders}
 
@@ -94,15 +123,15 @@ zoznam = Get_files_from_dir_by_extension(blast_xml_dir, "xml")
 for i, xml_file in enumerate(zoznam):
   print(f"FILE {xml_file}")
   prefix = xml_file.split('/')[-1].replace(".xml", "")
-  Parse_blast_xml(xml_file, prefix, blastHeaders, blastTable, pdbchTable)
+  Parse_blast_xml(xml_file, prefix, blastHeaders, blastTable, pdbchTable, new_version)
   print()
 
 print("Finished Parsing")
 
 df_blast = pd.DataFrame(blastTable)
-df_blast.to_excel('./' + 'searches.xlsx')
+df_blast.to_excel('./' + f'{dom_prot}_searches.xlsx')
 
 df_pdbch = pd.DataFrame(pdbchTable)
-df_pdbch.to_excel('./' + 'matches.xlsx')
+df_pdbch.to_excel('./' + f'{dom_prot}_matches.xlsx')
 
 
